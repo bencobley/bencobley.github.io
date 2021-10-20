b00a31af3f5bdc9cc1d47b3121ec99618c98e5ac
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2021-10-20 11:27:09 +0100

    Rationalise homepage and add inky

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-filterSelection("all") // Execute the function and show all columns
-
+filterSelection("Non"); // Execute the function and show all columns
 
 function filterSelection(c) {
   var x, i;
@@ -11,7 +10,6 @@ function filterSelection(c) {
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
   }
 }
-
 
 // Show filtered elements
 function w3AddClass(element, name) {
@@ -32,7 +30,7 @@ function w3RemoveClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
     }
   }
   element.className = arr1.join(" ");
@@ -42,7 +40,7 @@ function w3RemoveClass(element, name) {
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
+  btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
@@ -66,18 +64,18 @@ for (var i = 0; i < btns.length; i++) {
 //}
 //
 //function onPlayerReady(event) {
-//  
+//
 //  // bind events
 //  var playButton = document.getElementById("play-button");
 //  playButton.addEventListener("click", function() {
 //    player.playVideo();
 //  });
-//  
+//
 //  var pauseButton = document.getElementById("pause-button");
 //  pauseButton.addEventListener("click", function() {
 //    player.pauseVideo();
 //  });
-//  
+//
 //}
 //
 //// Inject YouTube API script
@@ -86,245 +84,276 @@ for (var i = 0; i < btns.length; i++) {
 //var firstScriptTag = document.getElementsByTagName('script')[0];
 //firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-
-
-$(document).ready(function(){
-  $('.slickcarousel').slick({
-          variableWidth: true,
-          dots: true,
-          infinite: false,
-          arrows: true,
-//          autoplay: true,
-//          autoplaySpeed: 3000,
-//          lazyLoad: 'ondemand',
-//          speed: 300,
-//          slidesToShow: 4,
-//          slidesToScroll: 4,
-//          slidesToShow: 1,
-          centerMode: true,
-//          height: 200,
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 4,
-                dots:false,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 768,
-              settings: {
-                arrows:false,
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                arrows:false,
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
-            }
-        ]
+$(document).ready(function () {
+  $(".slickcarousel").slick({
+    variableWidth: true,
+    dots: true,
+    infinite: false,
+    arrows: true,
+    //          autoplay: true,
+    //          autoplaySpeed: 3000,
+    //          lazyLoad: 'ondemand',
+    //          speed: 300,
+    //          slidesToShow: 4,
+    //          slidesToScroll: 4,
+    //          slidesToShow: 1,
+    centerMode: true,
+    //          height: 200,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          dots: false,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
 
-
-!function($){
-  
+!(function ($) {
   var defaults = {
-		animation: "dissolve",
-		separator: ",",
-		speed: 2000
-	};
-	
-	$.fx.step.textShadowBlur = function(fx) {
-    $(fx.elem).prop('textShadowBlur', fx.now).css({textShadow: '0 0 ' + Math.floor(fx.now) + 'px black'});
+    animation: "dissolve",
+    separator: ",",
+    speed: 2000,
   };
-	
-  $.fn.textrotator = function(options){
+
+  $.fx.step.textShadowBlur = function (fx) {
+    $(fx.elem)
+      .prop("textShadowBlur", fx.now)
+      .css({ textShadow: "0 0 " + Math.floor(fx.now) + "px black" });
+  };
+
+  $.fn.textrotator = function (options) {
     var settings = $.extend({}, defaults, options);
-    
-    return this.each(function(){
-      var el = $(this)
+
+    return this.each(function () {
+      var el = $(this);
       var array = [];
-      $.each(el.text().split(settings.separator), function(key, value) { 
-        array.push(value); 
+      $.each(el.text().split(settings.separator), function (key, value) {
+        array.push(value);
       });
       el.text(array[0]);
-      
+
       // animation option
-      var rotate = function() {
-        switch (settings.animation) { 
-          case 'dissolve':
-            el.animate({
-              textShadowBlur:20,
-              opacity: 0
-            }, 500 , function() {
-              index = $.inArray(el.text(), array)
-              if((index + 1) == array.length) index = -1
-              el.text(array[index + 1]).animate({
-                textShadowBlur:0,
-                opacity: 1
-              }, 500 );
-            });
-          break;
-          
-          case 'flip':
-            if(el.find(".back").length > 0) {
-              el.html(el.find(".back").html())
+      var rotate = function () {
+        switch (settings.animation) {
+          case "dissolve":
+            el.animate(
+              {
+                textShadowBlur: 20,
+                opacity: 0,
+              },
+              500,
+              function () {
+                index = $.inArray(el.text(), array);
+                if (index + 1 == array.length) index = -1;
+                el.text(array[index + 1]).animate(
+                  {
+                    textShadowBlur: 0,
+                    opacity: 1,
+                  },
+                  500
+                );
+              }
+            );
+            break;
+
+          case "flip":
+            if (el.find(".back").length > 0) {
+              el.html(el.find(".back").html());
             }
-          
-            var initial = el.text()
-            var index = $.inArray(initial, array)
-            if((index + 1) == array.length) index = -1
-            
+
+            var initial = el.text();
+            var index = $.inArray(initial, array);
+            if (index + 1 == array.length) index = -1;
+
             el.html("");
             $("<span class='front'>" + initial + "</span>").appendTo(el);
-            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
-            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip").show().css({
-              "-webkit-transform": " rotateY(-180deg)",
-              "-moz-transform": " rotateY(-180deg)",
-              "-o-transform": " rotateY(-180deg)",
-              "transform": " rotateY(-180deg)"
-            })
-            
-          break;
-          
-          case 'flipUp':
-            if(el.find(".back").length > 0) {
-              el.html(el.find(".back").html())
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(
+              el
+            );
+            el.wrapInner("<span class='rotating' />")
+              .find(".rotating")
+              .hide()
+              .addClass("flip")
+              .show()
+              .css({
+                "-webkit-transform": " rotateY(-180deg)",
+                "-moz-transform": " rotateY(-180deg)",
+                "-o-transform": " rotateY(-180deg)",
+                transform: " rotateY(-180deg)",
+              });
+
+            break;
+
+          case "flipUp":
+            if (el.find(".back").length > 0) {
+              el.html(el.find(".back").html());
             }
-          
-            var initial = el.text()
-            var index = $.inArray(initial, array)
-            if((index + 1) == array.length) index = -1
-            
+
+            var initial = el.text();
+            var index = $.inArray(initial, array);
+            if (index + 1 == array.length) index = -1;
+
             el.html("");
             $("<span class='front'>" + initial + "</span>").appendTo(el);
-            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
-            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip up").show().css({
-              "-webkit-transform": " rotateX(-180deg)",
-              "-moz-transform": " rotateX(-180deg)",
-              "-o-transform": " rotateX(-180deg)",
-              "transform": " rotateX(-180deg)"
-            })
-            
-          break;
-          
-          case 'flipCube':
-            if(el.find(".back").length > 0) {
-              el.html(el.find(".back").html())
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(
+              el
+            );
+            el.wrapInner("<span class='rotating' />")
+              .find(".rotating")
+              .hide()
+              .addClass("flip up")
+              .show()
+              .css({
+                "-webkit-transform": " rotateX(-180deg)",
+                "-moz-transform": " rotateX(-180deg)",
+                "-o-transform": " rotateX(-180deg)",
+                transform: " rotateX(-180deg)",
+              });
+
+            break;
+
+          case "flipCube":
+            if (el.find(".back").length > 0) {
+              el.html(el.find(".back").html());
             }
-          
-            var initial = el.text()
-            var index = $.inArray(initial, array)
-            if((index + 1) == array.length) index = -1
-            
+
+            var initial = el.text();
+            var index = $.inArray(initial, array);
+            if (index + 1 == array.length) index = -1;
+
             el.html("");
             $("<span class='front'>" + initial + "</span>").appendTo(el);
-            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
-            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip cube").show().css({
-              "-webkit-transform": " rotateY(180deg)",
-              "-moz-transform": " rotateY(180deg)",
-              "-o-transform": " rotateY(180deg)",
-              "transform": " rotateY(180deg)"
-            })
-            
-          break;
-          
-          case 'flipCubeUp':
-            if(el.find(".back").length > 0) {
-              el.html(el.find(".back").html())
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(
+              el
+            );
+            el.wrapInner("<span class='rotating' />")
+              .find(".rotating")
+              .hide()
+              .addClass("flip cube")
+              .show()
+              .css({
+                "-webkit-transform": " rotateY(180deg)",
+                "-moz-transform": " rotateY(180deg)",
+                "-o-transform": " rotateY(180deg)",
+                transform: " rotateY(180deg)",
+              });
+
+            break;
+
+          case "flipCubeUp":
+            if (el.find(".back").length > 0) {
+              el.html(el.find(".back").html());
             }
-          
-            var initial = el.text()
-            var index = $.inArray(initial, array)
-            if((index + 1) == array.length) index = -1
-            
+
+            var initial = el.text();
+            var index = $.inArray(initial, array);
+            if (index + 1 == array.length) index = -1;
+
             el.html("");
             $("<span class='front'>" + initial + "</span>").appendTo(el);
-            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(el);
-            el.wrapInner("<span class='rotating' />").find(".rotating").hide().addClass("flip cube up").show().css({
-              "-webkit-transform": " rotateX(180deg)",
-              "-moz-transform": " rotateX(180deg)",
-              "-o-transform": " rotateX(180deg)",
-              "transform": " rotateX(180deg)"
-            })
-            
-          break;
-          
-          case 'spin':
-            if(el.find(".rotating").length > 0) {
-              el.html(el.find(".rotating").html())
+            $("<span class='back'>" + array[index + 1] + "</span>").appendTo(
+              el
+            );
+            el.wrapInner("<span class='rotating' />")
+              .find(".rotating")
+              .hide()
+              .addClass("flip cube up")
+              .show()
+              .css({
+                "-webkit-transform": " rotateX(180deg)",
+                "-moz-transform": " rotateX(180deg)",
+                "-o-transform": " rotateX(180deg)",
+                transform: " rotateX(180deg)",
+              });
+
+            break;
+
+          case "spin":
+            if (el.find(".rotating").length > 0) {
+              el.html(el.find(".rotating").html());
             }
-            index = $.inArray(el.text(), array)
-            if((index + 1) == array.length) index = -1
-            
-            el.wrapInner("<span class='rotating spin' />").find(".rotating").hide().text(array[index + 1]).show().css({
-              "-webkit-transform": " rotate(0) scale(1)",
-              "-moz-transform": "rotate(0) scale(1)",
-              "-o-transform": "rotate(0) scale(1)",
-              "transform": "rotate(0) scale(1)"
-            })
-          break;
-          
-          case 'fade':
-            el.fadeOut(settings.speed, function() {
-              index = $.inArray(el.text(), array)
-              if((index + 1) == array.length) index = -1
+            index = $.inArray(el.text(), array);
+            if (index + 1 == array.length) index = -1;
+
+            el.wrapInner("<span class='rotating spin' />")
+              .find(".rotating")
+              .hide()
+              .text(array[index + 1])
+              .show()
+              .css({
+                "-webkit-transform": " rotate(0) scale(1)",
+                "-moz-transform": "rotate(0) scale(1)",
+                "-o-transform": "rotate(0) scale(1)",
+                transform: "rotate(0) scale(1)",
+              });
+            break;
+
+          case "fade":
+            el.fadeOut(settings.speed, function () {
+              index = $.inArray(el.text(), array);
+              if (index + 1 == array.length) index = -1;
               el.text(array[index + 1]).fadeIn(settings.speed);
             });
-          break;
+            break;
         }
       };
       setInterval(rotate, settings.speed);
     });
-  }
-  
-}(window.jQuery);
+  };
+})(window.jQuery);
 
 // Loader textrotator
-$(document).ready(function() {
-    $(".rotator .rotate").textrotator({
-        animation: "flipUp",
-        speed: 1750
-    });
+$(document).ready(function () {
+  $(".rotator .rotate").textrotator({
+    animation: "flipUp",
+    speed: 1750,
+  });
 
-                // Show page after 2s of loading animation
-                setTimeout(showPage, 1600);
-
+  // Show page after 2s of loading animation
+  setTimeout(showPage, 1600);
 });
-
 
 // Loader
 function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("myDiv").style.display = "block";
-    document.getElementById("myFooter").style.display = "block";
-    document.getElementById("myNav").style.display = "block";
-    document.getElementById("back-top").style.display = "block";
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+  document.getElementById("myFooter").style.display = "block";
+  document.getElementById("myNav").style.display = "block";
+  document.getElementById("back-top").style.display = "block";
 }
-
 
 // Accordion
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("accactive");
     var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
+    if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
 }
-
-
-
