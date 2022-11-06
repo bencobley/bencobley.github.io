175947c60b2713521e10d3298377a51100e4ff89
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2022-11-06 21:02:36 +0000

    Formatting improvements

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-    new Splide( '#image-carousel', {
-      heightRatio: 0.7,
-    } ).mount();
-  } );
+document.addEventListener("DOMContentLoaded", function () {
+  var elms = document.getElementsByClassName("splide");
 
+  for (var i = 0; i < elms.length; i++) {
+    new Splide(elms[i]).mount();
+  }
 
-  document.addEventListener( 'DOMContentLoaded', function () {
-    document.getElementById('mywrap').style.height = String(document.getElementById('mycol').offsetHeight) + "px"
-  } );
+  document.getElementById("dummy-section0").style.height = String(
+    Math.max(
+      document.getElementById("dummy-column0").offsetHeight,
+      window.innerHeight
+    ) + "px"
+  );
 
+  if (window.matchMedia("(min-width: 500px)").matches) {
+    document.getElementById("dummy-section1").style.height =
+      String(document.getElementById("dummy-column1").offsetHeight) + "px";
 
-  document.addEventListener( 'DOMContentLoaded', function () {
-    document.getElementById('mywrap2').style.height = String(document.getElementById('mycol2').offsetHeight) + "px"
-  } );
+    document.getElementById("dummy-section2").style.height =
+      String(document.getElementById("dummy-column2").offsetHeight) + "px";
 
-  document.addEventListener( 'DOMContentLoaded', function () {
-    document.getElementById('mywrap3').style.height = String(document.getElementById('mycol3').offsetHeight) + "px"
-  } );
+    document.getElementById("dummy-section3").style.height =
+      String(document.getElementById("dummy-column3").offsetHeight) + "px";
+  } else {
+    // console.log("Screen less than 500px");
+  }
+});
+
+$("section").each(function (index) {
+  $("section").each(function (index) {
+    console.log(index + ": " + $(this).class());
+  });
+});
