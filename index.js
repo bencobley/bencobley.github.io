70127f2de702b7abcaeb9997e2c17357d8eb6274
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2022-11-06 02:31:42 +0000

    Added timeline scrolling design

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '#image-carousel', {
-      heightRatio: 0.8,
+      heightRatio: 0.7,
     } ).mount();
+  } );
+
+
+  document.addEventListener( 'DOMContentLoaded', function () {
+    document.getElementById('mywrap').style.height = String(document.getElementById('mycol').offsetHeight) + "px"
+  } );
+
+
+  document.addEventListener( 'DOMContentLoaded', function () {
+    document.getElementById('mywrap2').style.height = String(document.getElementById('mycol2').offsetHeight) + "px"
+  } );
+
+  document.addEventListener( 'DOMContentLoaded', function () {
+    document.getElementById('mywrap3').style.height = String(document.getElementById('mycol3').offsetHeight) + "px"
   } );
