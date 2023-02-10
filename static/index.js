06a29591f5f5131bd77af43f2278813b6be3844e
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2023-02-10 12:37:40 -0500

    Add theme column to mobile

--- static/index.js
+++ static/index.js
@@ -13,21 +13,40 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function resizeSections() {
-    if ($(window).width() > 1000) {
+    // if ($(window).width() > 1000) {
+
+    // Calculate total height for each timeline-theme-row once page is loaded
+    $(".timeline-theme-row").each(function () {
       let totalHeight = 0;
-      $("row").each(function (index) {
-        let rowHeight = $(this)
-          .children("timeline-body")
-          .eq(0)
-          .outerHeight(true);
-        if (typeof rowHeight !== "undefined") {
-          $(this).height(rowHeight);
-          totalHeight += rowHeight;
-          console.log(rowHeight);
-        }
-      });
-      $("timeline-theme").height(totalHeight);
-      console.log(totalHeight);
-    }
+
+      // For each item row:
+      $(this)
+        .children(".timeline-item-column")
+        .children(".timeline-item-row")
+        .each(function () {
+          // Get the height of timeline-body
+          let bodyHeight = $(this).children("timeline-body").eq(0).outerHeight(true);
+          // Get the height of timeline-item-sticky
+          let stickyHeight = $(this).children(".timeline-item-sticky").eq(0).outerHeight(true);
+          // Set timeline row height depending on mobile or desktop
+          if ($(window).width() > 1000) {
+            $(this).height(bodyHeight);
+            totalHeight += bodyHeight;
+          } else {
+            $(this).height(bodyHeight + stickyHeight);
+            totalHeight += bodyHeight + stickyHeight;
+          }
+        });
+      // Set the height of the timeline-theme-row to the total height
+      $(this).height(totalHeight);
+    });
+    // }
   }
 });
+
+// Set timeline-theme-column height to the total height minus the text height
+// let textHeight = $(this).children(".timeline-theme-column").children(".timeline-theme-sticky").eq(0).outerHeight(true);
+// $(this)
+//   .children(".timeline-theme-column")
+//   .eq(0)
+//   .height(totalHeight - textHeight);
